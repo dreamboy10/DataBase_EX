da26f8d82f398a56bb5bf3fae4c27626468c3571
dreamboy10 <79038291+dreamboy10@users.noreply.github.com>
2022-01-10 16:28:08 +0900

    2022.01.10

--- 5장 SQL기초 실습.sql
+++
@@ -1,4 +0,0 @@
-SELECT version(); -- MySQL 버전 표시
-SELECT current_date(), current_time(), now(); -- 현재 날짜와 시간 표시
-CREATE USER 'manager'@'%' IDENTIFIED BY '333333';
-GRANT ALL ON *.* TO 'manager'@'%' WITH GRANT OPTION;
